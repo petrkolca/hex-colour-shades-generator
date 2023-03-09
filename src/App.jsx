c5c12af95fbdef76f3dd95ee12733a7088808fe6
Petr Kolarik <kolarik.petr@outlook.com>
2023-03-09 17:31:32 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Fragment, useState } from 'react'
+import { GlobalStyle } from './components/styles/GlobalStyle'
+
+function App() {
+
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <main>
+        <h2>Hex Color Generator</h2>
+      </main>
+    </Fragment>
+  )
+}
+
+export default App
