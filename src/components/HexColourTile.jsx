d3b7ff5fe4ada26ca0642c6ab9839869fdd8f6c3
Petr Kolarik <kolarik.petr@outlook.com>
2023-03-12 16:12:44 +1100

    Hex Color TIle - Copy to Clipboard JS func onClick

--- src/components/HexColourTile.jsx
+++ src/components/HexColourTile.jsx
@@ -4,14 +4,26 @@ import { getContrastColor, rgbToHex } from "./utils/utils";
 
 
 const HexColourTile = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false)
   const bgColor = rgb.join(",");
   const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
   const textColor = getContrastColor(hexColor);
 
+  //copy to clipboard function
+  const copyToClipHandler = () => {
+    setAlert(true);
 
+    //copy current HEX colour value to clipboard
+    navigator.clipboard.writeText(hex);
+  }
   return (
-    <ColorTile className={`color`} bcg={bgColor} txtColor={textColor}>
+    <ColorTile 
+      className={`color`} 
+      bcg={bgColor} 
+      txtColor={textColor}
+      onClick={copyToClipHandler}
+    >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hex}</p>
       {alert && <p className="alert">Copied to clipboard</p>}
